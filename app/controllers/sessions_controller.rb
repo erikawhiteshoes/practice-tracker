a66945930875bde38a5e871e157f00ed6fecdd91
erikawhiteshoes <erikawhiteshoes@gmail.com>
2016-11-09 19:48:54 -0500

    Added gems, forms and views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,41 @@
 class SessionsController < ApplicationController
+  before_action :find_session, only: [:show, :edit, :update, :destroy]
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @session = Session.new
+  end
+
+  def create
+    @session = Session.new(session_params)
+    if @session.save
+      redirect_to @session
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:date, :session, :mood, :duration)
+  end
+
+  def find_session
+    @session = Session.find(params[:id])
+  end
+
 end
