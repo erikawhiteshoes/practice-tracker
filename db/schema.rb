fc2341099f2f6cd3e3a8ecf4cab21fec5e1fc2dd
erikawhiteshoes <erikawhiteshoes@gmail.com>
2016-11-14 10:59:26 -0500

    added instruments

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104203352) do
+ActiveRecord::Schema.define(version: 20161114153341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "instruments", force: :cascade do |t|
+    t.string   "name"
+    t.string   "exercise"
+    t.string   "tempo"
+    t.string   "notes"
+    t.integer  "session_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "instruments", ["session_id"], name: "index_instruments_on_session_id", using: :btree
 
   create_table "sessions", force: :cascade do |t|
     t.datetime "date"
@@ -25,4 +37,5 @@ ActiveRecord::Schema.define(version: 20161104203352) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "instruments", "sessions"
 end
